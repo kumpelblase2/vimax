183c69b6d78400d52283b9454737a859fa85ed12
Tim Hagemann <tim@eternalwings.de>
2019-10-04 13:49:32 +0200

    Allow (de-)selecting all visible metadata at once

--- src/frontend/src/components/MetadataSelection.vue
+++ src/frontend/src/components/MetadataSelection.vue
@@ -1,6 +1,14 @@
 <template>
     <v-select class="nav-select single-line" :value="visibleMetadata" :items="orderedMetadata" item-text="name" return-object
               flat dense multiple @change="onlyShowMetadata" placeholder="No metadata visible" solo>
+        <template v-slot:prepend-item>
+            <v-list-tile @click="toggleAll">
+                <v-list-tile-content>
+                    Select All
+                </v-list-tile-content>
+            </v-list-tile>
+            <v-divider class="mt-2"></v-divider>
+        </template>
         <template v-slot:selection="{ item, index }">
             <span v-if="index === 2" class="grey--text">
                  &nbsp;+{{visibleMetadata.length - 2}} more
@@ -16,14 +24,25 @@
     import { mapGetters, mapActions } from 'vuex';
 
     export default {
+        name: "MetadataSelection",
         computed: {
+            allSelected() {
+                return this.visibleMetadata.length === this.orderedMetadata.length;
+            },
             ...mapGetters('settings/metadata', [
                 'visibleMetadata',
                 'orderedMetadata'
             ])
         },
         methods: {
-            ...mapActions('settings/metadata', ['onlyShowMetadata'])
+            ...mapActions('settings/metadata', ['onlyShowMetadata', 'showAllMetadata', 'hideAllMetadata']),
+            toggleAll() {
+                if(this.allSelected) {
+                    this.hideAllMetadata();
+                } else {
+                    this.showAllMetadata();
+                }
+            }
         }
     }
 </script>
