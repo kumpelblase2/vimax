e48d5da1415762233cc34f5bbd2cca2d6767c224
Tim Hagemann <tim@eternalwings.de>
2019-11-21 21:46:44 +0100

    Fix display of duration metadata values

--- src/frontend/src/helpers/metadata-display-helper.js
+++ src/frontend/src/helpers/metadata-display-helper.js
@@ -5,6 +5,7 @@ export function canBeDisplayedAsText(type) {
         case 'RANGE':
         case 'BOOLEAN':
         case 'SELECTION':
+        case 'DURATION':
             return true;
         default:
             return false;
@@ -35,10 +36,33 @@ export function toDisplayValue(type, value) {
                 return value ? 'Yes' : 'No';
             case 'SELECTION':
                 return value.name.toString();
+            case 'DURATION':
+                const parsedDuration = parseDurationString(value);
+                const daysPart = parsedDuration.days > 0 ? parsedDuration.days + "d " : "";
+                const hourPart = parsedDuration.hours > 0 ? parsedDuration.hours + "h " : "";
+                const minutesPart = parsedDuration.minutes > 0 ? parsedDuration.minutes + "m " : "";
+                const secondsPart = parsedDuration.seconds > 0 ? parsedDuration.seconds + "s " : "";
+                return daysPart + hourPart + minutesPart + secondsPart;
             default:
                 throw `No text representation for ${type}.`;
         }
     } else {
         return "";
     }
+}
+
+export function parseDurationString(durationString) {
+    const stringPattern = /^PT(?:(\d+)D)?(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d{1,3})?)S)?$/;
+    const stringParts = stringPattern.exec(durationString);
+
+    const days = stringParts[1] === undefined ? 0 : stringParts[1] * 1;
+    const hours = stringParts[2] === undefined ? 0 : stringParts[2] * 1;
+    const minutes = stringParts[3] === undefined ? 0 : stringParts[3] * 1;
+    const seconds = stringParts[4] === undefined ? 0 : stringParts[4] * 1;
+    return {
+        days,
+        hours,
+        minutes,
+        seconds
+    };
 }
