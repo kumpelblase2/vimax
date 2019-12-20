701d78d9ddb3c798eeb93c9e4818cc915208f619
Tim Hagemann <tim@eternalwings.de>
2019-12-20 15:46:24 +0100

    Fix migration
    It appears that this migration already references new fields while migrating old
     data before they were added. This is obviously a mistake and should be
    corrected.
    
    Additionally, flyway already creates a transaction so we don't need to create
    our own transaction in the script. This does throw an error which seems to have
    been missed.

--- src/main/resources/db/migration/V1.1__AddOrderToPlaylistVideos.sql
+++ src/main/resources/db/migration/V1.1__AddOrderToPlaylistVideos.sql
@@ -4,12 +4,11 @@ ALTER TABLE playlist_videos
 
 UPDATE playlist_videos
 SET position =
-        (SELECT rowid FROM playlist_videos pv WHERE pv.playlist_id = playlist_videos.playlist_id AND pv.video_id = playlist_videos.video_id) -
+        (SELECT rowid FROM playlist_videos pv WHERE pv.playlist_id = playlist_videos.playlist_id AND pv.videos_id = playlist_videos.videos_id) -
         (SELECT min(rowid) FROM playlist_videos pv WHERE pv.playlist_id = playlist_videos.playlist_id);
 
-BEGIN TRANSACTION;
 CREATE TEMPORARY TABLE playlist_videos_bak(playlist_id INTEGER, video_id INTEGER, position INTEGER);
-INSERT INTO playlist_videos_bak SELECT playlist_id, video_id, position FROM playlist_videos;
+INSERT INTO playlist_videos_bak SELECT playlist_id, videos_id as video_id, position FROM playlist_videos;
 DROP TABLE playlist_videos;
 CREATE TABLE playlist_videos(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -23,4 +22,3 @@ CREATE TABLE playlist_videos(
 );
 
 INSERT INTO playlist_videos(playlist_id, video_id, position) SELECT playlist_id, video_id, position FROM playlist_videos_bak;
-COMMIT;
