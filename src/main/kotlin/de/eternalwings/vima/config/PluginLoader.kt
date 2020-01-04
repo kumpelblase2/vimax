552d8957e561dda90e090a258f77bc4144501fa0
Tim Hagemann <tim@eternalwings.de>
2020-01-04 11:53:14 +0100

    Fix exception when plugin doesn't have registerPlugin at last place

--- src/main/kotlin/de/eternalwings/vima/config/PluginLoader.kt
+++ src/main/kotlin/de/eternalwings/vima/config/PluginLoader.kt
@@ -4,6 +4,7 @@ import de.eternalwings.vima.plugin.PluginBindings
 import de.eternalwings.vima.plugin.PluginConfig
 import de.eternalwings.vima.plugin.PluginManager
 import de.swirtz.ktsrunner.objectloader.KtsObjectLoader
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.io.File
 import java.io.Reader
@@ -28,12 +29,20 @@ class PluginLoader(private val pluginBindings: PluginBindings, private val plugi
             .filter { it != null }
             .forEach { script ->
                 val config = script!!.use { scriptLoader.load(it.reader(), bindings, PluginConfig::class.java) }
-                pluginManager.registerPlugin(config)
+                if(config == null) {
+                    LOGGER.warn("Couldn't load plugin, it didn't return a plugin config.")
+                } else {
+                    pluginManager.registerPlugin(config)
+                }
             }
 
         pluginManager.disableUnloaded()
     }
 
-    fun <T> KtsObjectLoader.load(reader: Reader, bindings: Bindings, returnType: Class<T>): T =
-            safeEval { engine.eval(reader, bindings) } as T
+    fun <T> KtsObjectLoader.load(reader: Reader, bindings: Bindings, returnType: Class<T>): T? =
+            safeEval { engine.eval(reader, bindings) } as T?
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(PluginLoader::class.java)
+    }
 }
