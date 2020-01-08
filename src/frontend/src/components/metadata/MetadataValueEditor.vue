52be5607630890f0235604938f17921b48850de3
Tim Hagemann <tim@eternalwings.de>
2020-01-08 18:20:17 +0100

    Make metadata value input fields require less space

--- src/frontend/src/components/metadata/MetadataValueEditor.vue
+++ src/frontend/src/components/metadata/MetadataValueEditor.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>
+    <div style="padding: 0">
         <v-combobox v-if="metadataDefinition.type === 'TEXT' && metadataDefinition.options.suggest" hide-no-data
                     :items="values" :label="metadataDefinition.name" :value="metadataValue"
                     :loading="isLoading" @change="update" :solo="solo"></v-combobox>
         <v-text-field v-else-if="metadataDefinition.type === 'TEXT' && !metadataDefinition.options.suggest"
                       :label="metadataDefinition.name" :value="metadataValue" @change="update" :solo="solo"></v-text-field>
         <v-switch v-else-if="metadataDefinition.type === 'BOOLEAN'" @change="update"
-                  :label="metadataDefinition.name" :input-value="metadataValue" :solo="solo"></v-switch>
+                  :label="metadataDefinition.name" :input-value="metadataValue" :solo="solo" dense></v-switch>
         <v-text-field v-else-if="metadataDefinition.type === 'NUMBER'" type="number"
                       :label="metadataDefinition.name" :value="metadataValue"
-                      @change="updateNumber" :solo="solo"></v-text-field>
+                      @change="updateNumber" :solo="solo" dense></v-text-field>
         <v-select v-else-if="metadataDefinition.type === 'SELECTION'" :label="metadataDefinition.name"
                   @change="update" :value="metadataValue" :items="metadataDefinition.options.values"
-                  item-text="name" return-object :solo="solo" :search-input.sync="search"></v-select>
+                  item-text="name" return-object :solo="solo" :search-input.sync="search" dense></v-select>
         <v-combobox v-else-if="metadataDefinition.type === 'TAGLIST'" :label="metadataDefinition.name" :value="metadataValue"
                     @change="update" chips clearable multiple :items="values" :solo="solo">
             <template v-slot:selection="{item, selected}">
