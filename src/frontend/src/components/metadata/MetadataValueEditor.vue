59dc58642620479750ab9cf3c4f1e42b10b0001c
Tim Hagemann <tim@eternalwings.de>
2020-01-11 11:04:15 +0100

    Use `deletable-chips` instead of custom chip component for tag inputs

--- src/frontend/src/components/metadata/MetadataValueEditor.vue
+++ src/frontend/src/components/metadata/MetadataValueEditor.vue
@@ -2,32 +2,22 @@
     <div style="padding: 0">
         <v-combobox v-if="metadataDefinition.type === 'TEXT' && metadataDefinition.options.suggest" hide-no-data
                     :items="values" :label="metadataDefinition.name" :value="metadataValue"
-                    :loading="isLoading" @change="update" :solo="solo"></v-combobox>
+                    :loading="isLoading" @change="update" :solo="solo"/>
         <v-text-field v-else-if="metadataDefinition.type === 'TEXT' && !metadataDefinition.options.suggest"
-                      :label="metadataDefinition.name" :value="metadataValue" @change="update" :solo="solo"></v-text-field>
+                      :label="metadataDefinition.name" :value="metadataValue" @change="update" :solo="solo"/>
         <v-switch v-else-if="metadataDefinition.type === 'BOOLEAN'" @change="update"
-                  :label="metadataDefinition.name" :input-value="metadataValue" :solo="solo" dense></v-switch>
+                  :label="metadataDefinition.name" :input-value="metadataValue" :solo="solo" dense/>
         <v-text-field v-else-if="metadataDefinition.type === 'NUMBER'" type="number"
                       :label="metadataDefinition.name" :value="metadataValue"
-                      @change="updateNumber" :solo="solo" dense></v-text-field>
+                      @change="updateNumber" :solo="solo" dense/>
         <v-select v-else-if="metadataDefinition.type === 'SELECTION'" :label="metadataDefinition.name"
                   @change="update" :value="metadataValue" :items="metadataDefinition.options.values"
-                  item-text="name" return-object :solo="solo" :search-input.sync="search" dense></v-select>
+                  item-text="name" return-object :solo="solo" :search-input.sync="search" dense/>
         <v-combobox v-else-if="metadataDefinition.type === 'TAGLIST'" :label="metadataDefinition.name" :value="metadataValue"
-                    @change="update" chips clearable multiple :items="values" :solo="solo">
-            <template v-slot:selection="{item, selected, parent}">
-                <v-chip :value="selected">
-                    <span class="pr-2">
-                        {{ item }}
-                    </span>
-                    <v-icon small @click="parent.selectItem(item)">close</v-icon>
-                </v-chip>
-            </template>
-        </v-combobox>
+                    @change="update" chips deletable-chips clearable multiple :items="values" :solo="solo"/>
         <v-slider v-else-if="metadataDefinition.type === 'RANGE'" :label="metadataDefinition.name"
                   :value="metadataValue" @change="updateNumber" :solo="solo" :min="metadataDefinition.options.min"
-                  :max="metadataDefinition.options.max" thumb-label>
-        </v-slider>
+                  :max="metadataDefinition.options.max" thumb-label/>
     </div>
 </template>
 
