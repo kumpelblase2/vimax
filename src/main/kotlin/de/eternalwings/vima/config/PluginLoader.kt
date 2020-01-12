85a51ee693dfb4d786237719b2da9ef0a62868b2
Tim Hagemann <tim@eternalwings.de>
2020-01-12 15:48:28 +0100

    Allow specifying external location for plugins
    Currently, the plugins were only loaded from inside the jar - or rather the
    classpath. This obviously isn't great for people who actually want to use this
    thing and therefor requires allowing plugins to sit outside of the application.
    This now adds a customizable folder location where a user can place a `*.kts`
    script and it will be loaded.
    
    Closes #7.

--- src/main/kotlin/de/eternalwings/vima/config/PluginLoader.kt
+++ src/main/kotlin/de/eternalwings/vima/config/PluginLoader.kt
@@ -2,25 +2,44 @@ package de.eternalwings.vima.config
 
 import de.eternalwings.vima.plugin.PluginManager
 import de.swirtz.ktsrunner.objectloader.KtsObjectLoader
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.core.io.ResourceLoader
 import org.springframework.core.io.support.ResourcePatternUtils
 import org.springframework.stereotype.Component
 import java.io.Reader
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.stream.Stream
 import javax.annotation.PostConstruct
 
 @Component
 @ConditionalOnProperty(value = ["disable-plugins"], havingValue = "false", matchIfMissing = true)
-class PluginLoader(private val pluginManager: PluginManager, private val resourceLoader: ResourceLoader) {
+class PluginLoader(private val pluginManager: PluginManager, private val resourceLoader: ResourceLoader,
+                   @Value("\${external-plugin-dir}") private val externalPluginDir: Path) {
+    private val matcher = FileSystems.getDefault().getPathMatcher("glob:**.kts")
     private val internalPluginDir = "plugins"
+
+    private val externalPluginFiles: Stream<Path>
+        get() {
+            return if (!Files.exists(externalPluginDir)) {
+                Stream.empty()
+            } else {
+                Files.list(externalPluginDir).filter { matcher.matches(it) }
+            }
+        }
 
     @PostConstruct
     fun loadPlugins() {
         val resources =
                 ResourcePatternUtils.getResourcePatternResolver(resourceLoader).getResources("classpath:$internalPluginDir/*.kts")
         val scriptLoader = KtsObjectLoader(this.javaClass.classLoader)
-        resources.forEach { script ->
-            script.inputStream.use { scriptLoader.execute(it.reader()) }
+        val resourceInputStreams = Stream.of(*resources).map { it.inputStream }
+        val externalInputStreams = externalPluginFiles.map { Files.newInputStream(it) }
+
+        Stream.concat(resourceInputStreams, externalInputStreams).forEach { script ->
+            script.use { scriptLoader.execute(it.reader()) }
         }
 
         pluginManager.disableUnloaded()
