ce355a08f73a566c68a7726c776fb0205558e256
Tim Hagemann <tim@eternalwings.de>
2020-01-12 12:20:35 +0100

    Use spring resource loader to load plugins
    When an artifact is built, the current way doesn't work as files can't seem to
    have a path. This now switches to using the spring provided resource loader to
    deal with included resources.

--- src/main/kotlin/de/eternalwings/vima/config/PluginLoader.kt
+++ src/main/kotlin/de/eternalwings/vima/config/PluginLoader.kt
@@ -3,30 +3,25 @@ package de.eternalwings.vima.config
 import de.eternalwings.vima.plugin.PluginManager
 import de.swirtz.ktsrunner.objectloader.KtsObjectLoader
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.core.io.ResourceLoader
+import org.springframework.core.io.support.ResourcePatternUtils
 import org.springframework.stereotype.Component
-import java.io.File
 import java.io.Reader
-import java.nio.file.Files
-import java.nio.file.Paths
 import javax.annotation.PostConstruct
 
 @Component
 @ConditionalOnProperty(value = ["disable-plugins"], havingValue = "false", matchIfMissing = true)
-class PluginLoader(private val pluginManager: PluginManager) {
+class PluginLoader(private val pluginManager: PluginManager, private val resourceLoader: ResourceLoader) {
     private val internalPluginDir = "plugins"
 
     @PostConstruct
     fun loadPlugins() {
-        val classLoader = Thread.currentThread().contextClassLoader
-        val classpathPluginDir = classLoader.getResource(internalPluginDir).path
-        val scriptLoader = KtsObjectLoader()
-        Files.list(Paths.get(classpathPluginDir)).filter { Files.isRegularFile(it) }
-            .filter { it.fileName.toString().endsWith("kts") }
-            .map { script -> classLoader.getResourceAsStream(internalPluginDir + File.separator + script.fileName.toString()) }
-            .filter { it != null }
-            .forEach { script ->
-                script!!.use { scriptLoader.execute(it.reader()) }
-            }
+        val resources =
+                ResourcePatternUtils.getResourcePatternResolver(resourceLoader).getResources("classpath:$internalPluginDir/*.kts")
+        val scriptLoader = KtsObjectLoader(this.javaClass.classLoader)
+        resources.forEach { script ->
+            script.inputStream.use { scriptLoader.execute(it.reader()) }
+        }
 
         pluginManager.disableUnloaded()
     }
