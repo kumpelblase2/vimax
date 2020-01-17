e300ad5e6d1005abdaaee046d030fb493d8421b8
Tim Hagemann <tim@eternalwings.de>
2020-01-17 17:13:11 +0100

    Whoops

--- src/main/kotlin/de/eternalwings/vima/config/FFmpegConfig.kt
+++ src/main/kotlin/de/eternalwings/vima/config/FFmpegConfig.kt
@@ -11,7 +11,7 @@ import java.lang.IllegalStateException
 @Configuration
 class FFmpegConfig {
     @Bean
-    fun ffmpeg(@Value("\${ffpmeg-binary:ffmpeg2}") ffmpegLocation: String): FFmpeg {
+    fun ffmpeg(@Value("\${ffpmeg-binary:ffmpeg}") ffmpegLocation: String): FFmpeg {
         try {
             return FFmpeg(ffmpegLocation)
         } catch (exception: IOException) {
