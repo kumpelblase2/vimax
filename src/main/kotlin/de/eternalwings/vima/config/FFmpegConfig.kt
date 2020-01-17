a94ad5451a043062bd2d98fc4220e43b6603963e
Tim Hagemann <tim@eternalwings.de>
2020-01-17 17:00:36 +0100

    Provide a little better error if ffmpeg isn't available

--- src/main/kotlin/de/eternalwings/vima/config/FFmpegConfig.kt
+++ src/main/kotlin/de/eternalwings/vima/config/FFmpegConfig.kt
@@ -5,12 +5,26 @@ import net.bramp.ffmpeg.FFprobe
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.IOException
+import java.lang.IllegalStateException
 
 @Configuration
 class FFmpegConfig {
     @Bean
-    fun ffmpeg(@Value("\${ffpmeg-binary:ffmpeg}") ffmpegLocation: String): FFmpeg = FFmpeg(ffmpegLocation)
+    fun ffmpeg(@Value("\${ffpmeg-binary:ffmpeg2}") ffmpegLocation: String): FFmpeg {
+        try {
+            return FFmpeg(ffmpegLocation)
+        } catch (exception: IOException) {
+            throw IllegalStateException("Couldn't find ffmpeg binary!")
+        }
+    }
 
     @Bean
-    fun ffprobe(@Value("\${ffprobe-binary:ffprobe}") ffprobeLocation: String): FFprobe = FFprobe(ffprobeLocation)
+    fun ffprobe(@Value("\${ffprobe-binary:ffprobe}") ffprobeLocation: String): FFprobe {
+        try {
+            return FFprobe(ffprobeLocation)
+        } catch (exception: IOException) {
+            throw IllegalStateException("Couldn't find ffprobe binary!")
+        }
+    }
 }
