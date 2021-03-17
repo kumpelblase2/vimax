c4775bdbbc0a92149f0fe0f77d02fabbfc4e1a02
Tim Hagemann <tim@eternalwings.de>
2021-03-17 12:05:08 +0100

    Only load a certain amount of items in the play queue
    To avoid loading a huge amount of videos when dealing with the play queue, we
    now only show N amount of videos before and after the playing video. This is
    similar to how plex handles it. For now, this is a static value of 20, so a
    total of 41 videos will be shown at most. An indicator showing that there are
    more elements will be added if more than the displayed videos are in the queue.
    
    For now this seems like the simplest solution but could, in the future, be
    replace with a virtualized list.

--- src/frontend/src/components/player/PlayQueue.vue
+++ src/frontend/src/components/player/PlayQueue.vue
@@ -1,9 +1,11 @@
 <template>
     <v-list style="max-height: 90%" class="overflow-y-auto">
+        <div v-show="hasMoreBefore" class="hidden-items-entry">...</div>
         <draggable handle=".drag-item" v-model="orderedVideos">
             <PlayQueueVideoItem v-for="videoId in orderedVideos" :key="videoId" :video-id="videoId"
                                 :current="currentVideoId === videoId"/>
         </draggable>
+        <div v-show="hasMoreAfter" class="hidden-items-entry">...</div>
     </v-list>
 </template>
 
@@ -11,6 +13,8 @@
     import PlayQueueVideoItem from "@/components/player/PlayQueueVideoItem";
     import draggable from 'vuedraggable';
     import { mapActions, mapGetters, mapMutations } from "vuex";
+
+    const AMOUNT_OF_VIDEOS_TO_SHOW_AROUND_CURRENT = 20;
 
     export default {
         name: "PlayQueue",
@@ -20,22 +24,50 @@
         },
         computed: {
             ...mapGetters('player', ['videoIdsInQueue', 'currentVideoId']),
+            videoDisplayIndex() {
+                return this.videoIdsInQueue.indexOf(this.currentVideoId);
+            },
+            startDisplayIndex() {
+                return Math.max(0, this.videoDisplayIndex - AMOUNT_OF_VIDEOS_TO_SHOW_AROUND_CURRENT);
+            },
+            endDisplayIndex() {
+                return Math.min(this.videoIdsInQueue.length - 1, this.videoDisplayIndex + AMOUNT_OF_VIDEOS_TO_SHOW_AROUND_CURRENT);
+            },
+            hasMoreBefore() {
+                return this.startDisplayIndex > 0;
+            },
+            hasMoreAfter() {
+                return this.endDisplayIndex < this.videoIdsInQueue.length - 1;
+            },
             orderedVideos: {
                 get() {
-                    return this.videoIdsInQueue;
+                    return this.videoIdsInQueue.slice(this.startDisplayIndex, this.endDisplayIndex + 1);
                 },
                 set(value) {
-                    this.updateOrder(value);
+                    this.updateOrderInSlice(value);
                 }
             }
         },
         methods: {
             ...mapActions('player', ['skipToVideo', 'removeVideo']),
             ...mapActions('videos', ['loadVideos']),
-            ...mapMutations('player', ['updateOrder'])
+            ...mapMutations('player', ['updateOrder']),
+            updateOrderInSlice(value) {
+                const copy = [...this.videoIdsInQueue];
+                copy.splice(this.startDisplayIndex, this.endDisplayIndex + 1, ...value);
+                this.updateOrder(copy);
+            },
+            loadVideosOfCurrentWindow() {
+                this.loadVideos(this.orderedVideos);
+            }
+        },
+        watch: {
+            orderedVideos(newValue) {
+                this.loadVideos(newValue);
+            }
         },
         mounted() {
-            this.loadVideos(this.videoIdsInQueue);
+            this.loadVideosOfCurrentWindow();
         }
     }
 </script>
@@ -43,5 +75,13 @@
 <style scoped>
     .v-list-item__title {
         padding-left: 10px
+    }
+
+    .hidden-items-entry {
+        height: 30px;
+        width: 100%;
+        text-align: center;
+        font-size: 3em;
+        transform: translateY(-30px);
     }
 </style>
