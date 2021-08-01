250d32a500e32119c85c7226daf90912739d2298
Tim Hagemann <tim@eternalwings.de>
2021-08-01 20:40:55 +0200

    Fix invalid metadata dto

--- src/main/kotlin/de/eternalwings/vima/dto/MetadataDTO.kt
+++ src/main/kotlin/de/eternalwings/vima/dto/MetadataDTO.kt
@@ -7,26 +7,28 @@ import de.eternalwings.vima.domain.PluginInformation
 import org.springframework.data.domain.Sort.Direction
 
 data class MetadataDTO(
-    val id: Int,
+    val id: Int?,
     val name: String,
     val type: MetadataType,
     val ordering: Direction,
     val readOnly: Boolean,
     val owner: PluginInformation?,
     val options: MetadataOptions<*,*>,
-    val displayOrder: Int
+    val displayOrder: Int,
+    val version: Int?
 ) {
     companion object {
         fun fromMetadata(metadata: Metadata): MetadataDTO {
             return MetadataDTO(
-                metadata.id!!,
+                metadata.id,
                 metadata.name!!,
                 metadata.type!!,
                 metadata.ordering,
                 metadata.readOnly,
                 metadata.owner?.copy(),
                 metadata.options!!,
-                metadata.displayOrder
+                metadata.displayOrder,
+                metadata.version
             )
         }
     }
